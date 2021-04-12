f529fe292db4e82fdb0aca97c798606efee8c2c1
Maanav Dalal <dalalm1@mcmaster.ca>
2021-04-12 17:08:17 -0400

    Created and tested CreditInterface.
    Co-authoried-by: Shazil Arif <shazilarif94@gmail.com>

--- app/src/CreditInterface.js
+++ app/src/CreditInterface.js
@@ -1,27 +1,59 @@
-class CreditInteface{
-    constructor(){
-        this.credits = 0;
-    }
+import React, { useEffect } from "react";
+class CreditInterface {
+	constructor() {
+		this.credits = 0;
+		this.history = [];
+	}
 
-    addCredits(credits){
-        this.credits += credits;
-    }
+	addCredits(credits, game) {
+		this.credits += credits;
+		this.history.push({ credits: credits, game: game });
+	}
 
-    removeCredits(credits){
-        this.credits -= credits;
-    }
+	removeCredits(credits, game) {
+		this.credits -= credits;
+		this.history.push({ credits: credits, game: game });
+	}
 
-    cashOut(){
+	cashOut() {}
 
-    }
+	generateReport = (arr) => {
+		let report = [];
+		arr.map((item) => {
+			let credits = item.credits;
+			let game = item.game;
+			report.push(
+				`You ${credits >= 0 ? "won" : "lost"} ${credits} in ${game}.`
+			);
+		});
+		return report;
+	};
+	getEarningsHistory() {
+		let storedEarnings = localStorage.getItem("creditReport");
+		let earnings = storedEarnings
+			.split("\n")
+			.slice(0, -1)
+			.map((item) => {
+				return JSON.parse(item);
+			});
+		return this.generateReport(earnings);
+	}
 
-    getEarningsHistory(){
+	saveCreditsLocally() {
+		let string = localStorage.getItem("creditReport");
+		this.history.forEach((item) => {
+			string += JSON.stringify(item) + "\n";
+		});
+		localStorage.setItem("creditReport", string);
+	}
 
-    }
+	printEarnings() {
+		return this.generateReport(this.history);
+	}
 
-    saveCreditsLocally(){
-
-    }
+	resetCredits() {
+		this.credits = 0;
+	}
 }
 
-export default new CreditInteface();
+export default new CreditInterface();
