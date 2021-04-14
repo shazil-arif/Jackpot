c5c8b2753462892fbdc321e0db99d84f4595e0bb
Vlad Dracula <gongc12@mcmaster.ca>
2021-04-14 09:24:09 -0400

    App.js fixed

--- app/src/App.js
+++ app/src/App.js
@@ -14,13 +14,6 @@ import { useState } from "react";
 import React from "react";
 
 function App() {
-<<<<<<< HEAD
-  const [credits, setCredits] = useState(CreditInterface.getCredits());
-  let oc = () => {
-    CreditInterface.addCredits(10, "Home");
-    setCredits(CreditInterface.getCredits());
-  };
-
 	// const _credits = useContext(CreditContext);
 
 	const [credits, setCredits] = useState(CreditInterface.getCredits());
@@ -29,45 +22,14 @@ function App() {
 		setCredits(CreditInterface.getCredits());
 	};
 
+	let oc2 = () => {
+		console.log(CreditInterface.printEarnings());
+	};
 
-  let oc2 = () => {
-    console.log(CreditInterface.printEarnings());
-  };
+	let oc3 = () => {
+		console.log(CreditInterface.getCredits());
+	};
 
-  let oc3 = () => {
-    console.log(CreditInterface.getCredits());
-  };
-
-  function Wrapper(Component) {
-    return function () {
-      return <Component setCredits={setCredits}></Component>;
-    };
-  }
-
-  return (
-    <div className="App">
-      <div>
-        <Link to="/">Home </Link>
-        <Link to="/twodots"> Two Dots </Link>
-        <Link to="/mastermind">Mastermind </Link>
-        <Link to="/sudoku">Sudoku </Link>
-        <Link to="/slots">Slots </Link>
-        <Link to="/russianroulette">Russian Roulette </Link>
-        <button onClick={oc}>Add </button>
-        <button onClick={oc2}>Print </button>
-        <button onClick={oc3}>Update </button>
-      </div>
-      <Switch>
-        <Route exact path="/" component={MainView} />
-        <Route exact path="/twodots" component={Wrapper(TwoDotsView)} />
-        <Route exact path="/mastermind" component={Wrapper(MasterMindView)} />
-        <Route exact path="/russianroulette" component={RussiaRouletteView} />
-        <Route exact path="/sudoku" component={SudokuView} />
-        <Route exact path="/slots" component={SlotsView} />
-      </Switch>
-      <p>Current Credits: {credits}</p>
-    </div>
-  );
 	function Wrapper(Component) {
 		return function () {
 			return <Component setCredits={setCredits}></Component>;
