11a127030b5ac0ee87ca239bbdb18349b02083bb
Maanav Dalal <dalalm1@mcmaster.ca>
2021-04-13 22:26:07 -0400

    Updated slots, will add credits next.

--- app/src/agents/Slots/SlotsView.js
+++ app/src/agents/Slots/SlotsView.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import Spinner from "./Spinner";
+import uniq from "lodash/uniq";
 
-const MAX_PRIZE = 100;
+const MAX_PRIZE = 50;
 const CONSEC_PRIZE = 20;
 const NON_CONSEC_PRIZE = 10;
 
 export const SlotsView = () => {
 	const [isRunning, setIsRunning] = useState(false);
-	const [winner, setWinner] = useState(false);
+	const [winner, setWinner] = useState("Play a round to get your results!");
 	const [credits, setCredits] = useState(0);
 
 	const handleStart = () => {
 		setIsRunning(true);
+		// Deduct credits
 	};
 
 	const handleStop = () => {
@@ -20,10 +22,33 @@ export const SlotsView = () => {
 	};
 
 	const handleResult = (wheels) => {
+		const images = wheels.map((wheel) => wheel.split("/").pop());
+		console.log(uniq(images));
+		const result = uniq(images);
 		// Lose
+		if (result.length == 3) {
+			setWinner("Sorry, you lost!");
+			// Update credits
+		}
 		// Win Lot
-		// Win Little
-		// Win even less because nonconsecutive
+		else if (result.length == 1) {
+			if (result[0].split(".")[0] == "banana") {
+				setWinner("You won the largest prize! Congrats!");
+				// Update credits
+			} else {
+				setWinner("You won a large prize!");
+				// Update credits
+			}
+		}
+		// Win Little (2)
+		else if (images[0] == images[1] || images[1] == images[2]) {
+			setWinner("You won a small prize!");
+			// Update credits
+		} else {
+			// Win even less because nonconsecutive
+			setWinner("You won a tiny prize. Not bad.");
+			// Update credits
+		}
 	};
 	return (
 		<div>
@@ -36,7 +61,7 @@ export const SlotsView = () => {
 			<Button onClick={handleStop} disabled={!isRunning}>
 				Stop
 			</Button>
-			<p>This will update based on rewards!</p>
+			<p>{winner}</p>
 		</div>
 	);
 };
